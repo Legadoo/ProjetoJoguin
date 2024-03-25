60cec3c56e4d69d16a0e2222963f604d99e76065
ErickGodoi <erikviniciusgodoi@gmail.com>
2024-03-25 16:17:53 -0300

    trablho de vagabundo

--- Personagem.java
+++ Personagem.java
@@ -1,10 +1,16 @@
 package packagejhon;
 
 public class Personagem {
-	public int vida = 10;
-	public float speed = 100f;
-	
-	
-	
+    public int vida = 10;
+    public Float speed = 100f;
+    public String nome = "Jonathan";
+    public Boolean vivo = false;
+    public Boolean trabalhou;
 
+    void FezTrabalho(){
+        if (!vivo){
+            trabalhou = false;
+        }
+        else trabalhou = true;
+    }
 }
